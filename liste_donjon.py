1bee24721e580b0460fb2bd5abaf59ab32bdef76
LordGolgotha <LordGolgotha@gmail.com>
2026-04-25 14:16:00 +0200

    Création d'un fichier contenant les listes des sonjons par tranches de levels

---
+++ liste_donjon.py
@@ -0,0 +1,158 @@
+from typing import Literal
+
+liste_donjon_20 = Literal[
+    "Pâturages Des Bouftous",
+    "Le Tofulailler",
+    "Larventura"
+]
+liste_donjon_35 = Literal[
+    "Donjon des Abraknes",
+    "Le Caveau Relevé",
+    "Piou Lahoupe"
+]
+liste_donjon_50 = Literal[
+    "Le Papaturage Royal",
+    "La Montagne Adezieu",
+    "Donjon Equipage Du Poulpe",
+    "Les Champs Pourchan",
+    "Donjon Morts-Brûlés",
+    "La Tour Minérale 50"
+]
+liste_donjon_65 = Literal[
+    "L'Antre Oubliée",
+    "Donjon Bwork",
+    "La Skouale Séchée",
+    "Donjon Mollusky",
+    "Kokokolantha",
+    "Château des Cwabes",
+    "Donjon Marteaux-Aigris",
+    "Tour des Miss Moches"
+]
+liste_donjon_80 = Literal[
+    "Les Ratacombes",
+    "La Corbeau-Cave",
+    "Domaine du Petit Groin",
+    "Palais du Tsu",
+    "Donjon Mulou",
+    "Antre d'Excarnus",
+    "Donjon Arakne",
+    "Truchière Abandonnée",
+    "Temple du grand Orrok"
+]
+liste_donjon_95 = Literal[
+    "Le Hammamamoule",
+    "Sliptorium",
+    "Le Misolée",
+    "Scarrière Abandonnée",
+    "Caverne des Slekymoses",
+    "Château de Wagnar",
+    "Académie Trool",
+    "Nécropoil de Morbax",
+    "Antre du Meulou"
+]
+liste_donjon_110 = Literal[
+    "Le Glaglacier Cornu",
+    "Niche du Yech'Ti'Wawa",
+    "Repaire des Magik Riktus",
+    "Chuchobase",
+    "Repaire des Super-Vilains",
+    "L'Arène Dansante",
+    "Donjon Gelée",
+    "Antre du Corbeau Noir",
+    "Antre du Boufrog"
+]
+liste_donjon_125 = Literal[
+    "Le pot d'Hagen-Glass",
+    "Donjon flaqueux",
+    "Caverne Smarrante",
+    "Fosse du Tourmenteur",
+    "Donjon Cacterre",
+    "La Tour Gelée 125",
+    "Aile de L'Ambassadrice",
+    "La Pichine",
+    "Antre du Dragon-Cochon"
+]
+liste_donjon_140 = Literal[
+    "Domaine de la Trouffe Salée",
+    "Donjon Noirespore",
+    "Sanctuaire de Mihmol",
+    "Donjon Abraknyde",
+    "Compost du grand Potofeu",
+    "Palais Lenald",
+    "Jawdin de la Weine"
+]
+liste_donjon_155 = Literal[
+    "Le Vignoble Ignoble",
+    "Donjon Srambad",
+    "Donjon Enutrosor",
+    "Donjon Blopéra",
+    "Source du Mal",
+    "Donjon Sabléoptère",
+    "Laboratoire de Womewo",
+    "Château du Wa Wabbit",
+    "Tal Kasha"
+]
+liste_donjon_170 = Literal[
+    "Donjon Patapoutrerie",
+    "Donjon Lampionaute",
+    "Donjon Roub Bar",
+    "Donjon Riktus Elite",
+    "Donjon E-Bou",
+    "Fabrique Méka",
+    "Repaire de Kali",
+    "Plateau des Haut-Vents: Serre d'Acier"
+]
+liste_donjon_185 = Literal[
+    "Donjon des Crocodailles",
+    "Donjon des Kannivores",
+    "Donjon des Tropikes",
+    "Donjon des Kannibouls",
+    "Cité Interdite",
+    "Cime du Grand Totem"
+]
+liste_donjon_200 = Literal[
+    "Sanctuaire des Dragoeufs",
+    "Tombeau de Pandala",
+    "La Crête Givrée",
+    "Canyon des Fléopards",
+    "Volcan Or'Hodruin",
+    "Tanière des Blérox",
+    "Usine Hibourg",
+    "Ogrest 200",
+    "Antre de Nogord l'Ezarélé",
+    "Dimension-Objet d'Ombrage",
+    "Tour Minérale 200"
+]
+liste_donjon_215 = Literal[
+    "Donjon Carapattes",
+    "Donjon Plantigardes",
+    "Donjon Mansots",
+    "Donjon Vandaliénés",
+    "Donjon Crustargneux",
+    "Donjon Cagnardeurs",
+    "Donjon Toundrasoirs",
+    "Donjon Tour Minérale 215",
+    "Ogrest 215"
+]
+liste_donjon_230 = Literal[
+    "Donjon Phytomorphe",
+    "Donjon Vidéants",
+    "Donjon Démhorribles",
+    "Donjon Vidéants",
+    "Ogrest 230",
+    "Donjon Égarés",
+    "Donjon Ravageurs",
+    "Palais de Rushu",
+    "Nécromonde",
+    "Donjon Steamers",
+    "Donjon Poisseux abyssaux"
+]
+liste_donjon_245 = Literal[
+    "Théatre intemporel",
+    "Donjon Machine de Nox",
+    "Donjon Clan de Bworkana",
+    "Donjon Savanastraux",
+    "Donjon Primassifs",
+    "Donjon Férociraptors",
+    "Coeur de l'horloge de Nox"
+]
