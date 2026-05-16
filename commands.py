a0f8ed2b0d7f40060780cc8f9c377f0a6851d74c
LordGolgotha <LordGolgotha@gmail.com>
2026-05-16 11:11:45 +0200

    Amélioration de la manière de lister les levels

--- commands.py
+++ commands.py
@@ -10,6 +10,8 @@ from liste_donjon import *
 
 load_dotenv()
 
+list_levels = Literal[20,35,50,65,80,95,110,125,140,155,170,185,200,215,230,245]
+
 emoji_list = [
     '<:osamodas:1483790487356706868>',
     '<:enutrof:1483790486023176372>',
@@ -102,7 +104,7 @@ async def addlowcost(ctx: discord.context_managers,classes : Classe,levels: str)
     brief = "Joueur opti pour un certain niveau.",
     help = "Mettez la tranche de niveau recherché pour obtenir tout les joueurs avec un stuff opti à ce niveau. Exemple /getopti 200"
     )
-async def getopti(ctx: discord.context_managers,levels : Literal[20,35,50,65,80,95,110,125,140,155,170,185,200,215,230,245]):
+async def getopti(ctx: discord.context_managers,levels : list_levels):
     print(f"recherche joueur dans la tranche {levels}")
     await ctx.defer()
     dict_player = get_opti(int(levels))
@@ -122,7 +124,7 @@ async def getopti(ctx: discord.context_managers,levels : Literal[20,35,50,65,80,
     brief = "Joueur low cost pour un certain niveau.",
     help = "Mettez la tranche de niveau recherché pour obtenir tout les joueurs avec un stuff low cost à ce niveau. Exemple /getlowcost 200"
     )
-async def getlowcost(ctx: discord.context_managers,levels : Literal[20,35,50,65,80,95,110,125,140,155,170,185,200,215,230,245]):
+async def getlowcost(ctx: discord.context_managers,levels : list_levels):
     print(f"recherche joueur dans la tranche {levels}")
     await ctx.defer()
     dict_player = get_low_cost(int(levels))
@@ -142,7 +144,7 @@ async def getlowcost(ctx: discord.context_managers,levels : Literal[20,35,50,65,
     brief = "Joueur low cost et opti pour un certain niveau.",
     help = "Mettez la tranche de niveau recherché. Exemple /getstuffer 200"
     )
-async def getstuffer(ctx: discord.context_managers,levels : Literal[20,35,50,65,80,95,110,125,140,155,170,185,200,215,230,245]):
+async def getstuffer(ctx: discord.context_managers,levels : list_levels):
     print(f"recherche joueur dans la tranche {levels}:")
     await ctx.defer()
     dict_player = get_stuffer(int(levels))
@@ -473,7 +475,6 @@ async def on_reaction_add(reaction, user):
         for joueur in liste:
             liste_mention.append(bot.get_user(joueur.id).mention)
         await message.reply(f"{','.join(liste_mention)} votre donjon est prêt !")
-    
 
 @bot.event
 async def on_ready():
