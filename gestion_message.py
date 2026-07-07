64144b45e3fd0a5d0b9abbe577f4fdc0c8261a01
LordGolgotha <LordGolgotha@gmail.com>
2026-07-08 01:13:27 +0200

    Ajustement des couleurs

--- gestion_message.py
+++ gestion_message.py
@@ -3,17 +3,22 @@ import time
 
 def construction_message(bot,id):
         info_dj = get_dj_info(id)
-        text = f"Donjon **{info_dj['donjon']}** modulé au niveau de stasis **S{info_dj['stasis']}**"
+        text = f"Donjon **{info_dj['donjon']}** modulé **S{info_dj['stasis']}**"
         nb_joueur = len(info_dj['joueurs'])
         if info_dj['date'] == "invalid":
-            text += f"date invalide"
+            text += f" date entrée invalide"
         elif info_dj['date'] != "":
             text += f" le <t:{info_dj['date']}:f> <t:{info_dj['date']}:R>"
         if info_dj['info'] != "":
             text += f"\n __**Info**__ : {info_dj['info']}"
-        color = "33m" if nb_joueur != 6 else "32m"
+        if 8 < nb_joueur < 4 :
+             color = "31m"
+        elif nb_joueur == 6:
+            color = "32m"
+        else:
+            color = "33m"
         text += f"\nChoisissez votre classe :"
-        text += f"\n```ansi\n\u001b[1;{color}{nb_joueur}/{info_dj['nb_joueur']} joueurs\u001b[0m\n```"
+        text += f"\n```ansi\n\u001b[1;{color}{nb_joueur}/{info_dj['nb_joueur']} joueurs\u001b[0m```"
         for j in info_dj['joueurs']:
             text += f"\n- {bot.get_user(j[0]).mention}: {str.capitalize(j[1])} {get_compo(id,j[0])}"
         return text
