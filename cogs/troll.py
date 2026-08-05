02e38c5e2a0e8015b1d33b06f047fafa0c13c40f
LordGolgotha <LordGolgotha@gmail.com>
2026-08-05 04:50:42 +0200

    ben

--- cogs/troll.py
+++ cogs/troll.py
@@ -8,7 +8,7 @@ class TrollCog(commands.Cog):
 
     @commands.hybrid_command(description="Bonjour?")
     async def bonjour(self, ctx):
-        await ctx.send(f"Bonsoir c'est moi Lastumus, je déteste les péruches, le hockey et ma soeur. Par contre j'adore les Tacos!")
+        await ctx.send(f"Bonsoir c'est moi Lastumus, je déteste les péruches, le hockey et ma soeur. Par contre j'adore les Tacos et Ben-Chest!")
 
 async def setup(bot):
     await bot.add_cog(TrollCog(bot))
