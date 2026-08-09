22142303412ca4f98f860aa8752a47c3c3d7396d
LordGolgotha <LordGolgotha@gmail.com>
2026-08-10 01:07:37 +0200

    finalisation

--- cogs/troll.py
+++ cogs/troll.py
@@ -41,6 +41,8 @@ class TrollCog(commands.Cog):
         self.liste_img_2 = list()
         self.liste_img_3 = list()
         self.liste_img_4 = list()
+        self.liste_img_5 = list()
+        self.liste_img_6 = list()
         for filename in os.listdir('./images/1'):
             if filename.endswith('.png'):
                 self.liste_img_1.append(filename)
@@ -53,6 +55,12 @@ class TrollCog(commands.Cog):
         for filename in os.listdir('./images/4'):
             if filename.endswith('.png'):
                 self.liste_img_4.append(filename)
+        for filename in os.listdir('./images/5'):
+            if filename.endswith('.png'):
+                self.liste_img_5.append(filename)
+        for filename in os.listdir('./images/6'):
+            if filename.endswith('.png'):
+                self.liste_img_6.append(filename)
 
     @commands.hybrid_command(description="Bonjour?")
     async def bonjour(self, ctx):
@@ -78,6 +86,12 @@ class TrollCog(commands.Cog):
         elif elem[1] == 4:
             r2 = randint(0, len(self.liste_img_4)-1)
             file = discord.File(f"./images/4/{self.liste_img_4[r2]}")
+        elif elem[1] == 5:
+                r2 = randint(0, len(self.liste_img_5)-1)
+                file = discord.File(f"./images/5/{self.liste_img_5[r2]}")
+        elif elem[1] == 6:
+                r2 = randint(0, len(self.liste_img_6)-1)
+                file = discord.File(f"./images/6/{self.liste_img_6[r2]}")
         await ctx.send(phrase,file=file)
 
 
